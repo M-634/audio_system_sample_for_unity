5c74e78c0ed289ccf9adfe5681d36a2af2c6e3c1
M-634 <miyanagateru634@gmail.com>
2021-10-21 20:15:26 +0900

    import TextMeshPro and auto save audio volume

---
+++ Assets/AudioSystem/Scripts/UGUIListnerExtension.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine.Events;
+using UnityEngine.UI;
+
+
+namespace M_634.UGUIExtension
+{
+    /// <summary>
+    /// UnityEngine.UIに含まれる各種クラスの拡張クラス
+    /// </summary>
+    public static class UGUIListnerExtension
+    {
+        public static void SetValueChangedEvent(this Slider slider,UnityAction<float> sliderCallback)
+        {
+            if (slider.onValueChanged != null)
+            {
+                slider.onValueChanged.RemoveAllListeners();
+            }
+            slider.onValueChanged.AddListener(sliderCallback);
+        }
+
+        public static void SetSliderValue(this Slider slider, float value)
+        {
+            slider.value = value;
+        }
+    }
+}
